2de832e0010ef8f65dcfbde19169b9103a91898f
Hsi-An Liu Moore <hsian@liumoore.com>
2015-03-14 23:27:54 -0500

    added front end

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var _ = require("lodash");
 var http = require("http");
 var pictures = require("./pictures");
 var movies = require("./movies");
+var fs = require("fs");
 
 var server = http.createServer(function(req, res){
     console.log("I got something ");
-    res.writeHead(200, {"Content-Type": "application/json"});
 
     var response = '{}';
     var data = '';
@@ -13,12 +13,17 @@ var server = http.createServer(function(req, res){
     console.log("urlArr", urlArr);
     if (req.method == 'GET') {
         if (urlArr[1] == 'pictures') {
+            res.writeHead(200, {"Content-Type": "application/json"});
             response = pictures.get();
         } else if (urlArr[1] == 'movies') {
+            res.writeHead(200, {"Content-Type": "application/json"});
             response = movies.get();
+        } else {
+            response = getIndex();
         }
         res.end(response);
     } else if (req.method == 'POST') {
+        res.writeHead(200, {"Content-Type": "application/json"});
         /* TODO http://blog.frankgrimm.net/2010/11/howto-access-http-message-body-post-data-in-node-js/ */
         req.on('data', function(chunk) {
             data += chunk.toString();
@@ -33,6 +38,7 @@ var server = http.createServer(function(req, res){
             res.end(response);
         });
     } else if (req.method == 'DELETE') {
+        res.writeHead(200, {"Content-Type": "application/json"});
         if (urlArr[1] == 'pictures') {
             response = pictures.delete(urlArr[2]);
         } else if (urlArr[1] == 'movies') {
@@ -40,6 +46,7 @@ var server = http.createServer(function(req, res){
         }
         res.end(response);
     } else if (req.method == 'PUT') {
+        res.writeHead(200, {"Content-Type": "application/json"});
         req.on('data', function(chunk) {
             data += chunk.toString();
             console.log("chunk: ", chunk.toString());
@@ -55,6 +62,10 @@ var server = http.createServer(function(req, res){
 
     }
 });
+
+function getIndex() {
+    return fs.readFileSync('./index.html');
+}
 
 server.listen(8080);
 
