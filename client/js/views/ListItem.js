3d38733963d2c187596160e0d3a36a05fc363a04
Hsi-An Liu Moore <hsian@liumoore.com>
2015-07-18 22:07:31 -0500

    added delete by Dustin Moore

--- client/js/views/ListItem.js
+++ client/js/views/ListItem.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
+var request = require('request');
 var View = require('./View');
+var events = require('../libs/events');
 
 function ListItem(movie) {
     View.call(this);
@@ -15,8 +17,19 @@ ListItem.prototype.template = fs.readFileSync(__dirname + '/ListItem.html', 'utf
 ListItem.prototype.render = function(data) {
     View.prototype.render.call(this);
 
+    this.key = data.key;
     this.el.querySelector('.name').innerText = data.filename;
     this.el.querySelector('.size').innerText = data.size;
+
+    this.deleteButton = this.el.querySelector('.delete');
+
+    this.deleteButton.addEventListener('click', this.delete.bind(this));
+};
+
+ListItem.prototype.delete = function() {
+    request.del({url: 'http://localhost:8080/movies/' + this.key}, function(err, res, data) {
+        events.trigger('refresh-list');
+    }.bind(this));
 };
 
 
